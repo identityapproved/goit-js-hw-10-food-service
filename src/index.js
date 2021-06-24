6d7e8edf889033f8e54b12bf0309577f825ce0a3
identityapproved <identityapproved@gmail.com>
2021-06-24 19:00:41 +0300

    theme-toggle/handlebars

--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 import menu from './menu.json';
+import template from './templates/events.hbs';
+
+
+
+console.log(template)
+
+const menuBoard = document.querySelector('.js-menu');
+
+menuBoard.innerHTML = template(menu);
+
+const checkboxToggleEl = document.querySelector('.theme-switch__toggle');
+
+checkboxToggleEl.addEventListener('change', checkboxThemeHandler)
+
+function checkboxThemeHandler() {
+   if (checkboxToggleEl.checked) {
+      handlerCheckedOn()
+   }
+   if (!checkboxToggleEl.checked) {
+      handlerCheckedOff()
+   }
+}
+
+function handlerCheckedOn() {
+   document.body.classList.add('dark-theme');
+   document.body.classList.remove('light-theme');
+   localStorage.setItem('theme', JSON.stringify('dark-theme'));
+   checkboxToggleEl.checked = true;
+}
+
+function handlerCheckedOff() {
+   document.body.classList.add('light-theme');
+   document.body.classList.remove('dark-theme');
+   localStorage.setItem('theme', JSON.stringify('light-theme'))
+   checkboxToggleEl.checked = false;
+}
+
+function defaultTheme() {
+   const currentTheme = localStorage.getItem('theme');
+   const parsedTheme = JSON.parse(currentTheme);
+
+   if (!currentTheme || parsedTheme === 'light-theme') {
+      handlerCheckedOff();
+   }
+   if (parsedTheme === 'dark-theme') {
+      handlerCheckedOn();
+   }
+}
+
+defaultTheme();
 
